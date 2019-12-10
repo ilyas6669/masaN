5e4a4fa40132e70eef4c1b6da0a7d9b41caa3566
İlyas Abiyev <999.ilyas.6666@gmail.com>
2019-12-10 21:45:11 +0400

    Initial Commit

---
+++ Nasam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nasam
+//
+//  Created by İlyas Abiyev on 12/10/19.
+//  Copyright © 2019 İlyas Abiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
